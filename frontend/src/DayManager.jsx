56af32d3a355afe010172bd68419ca8c32cce51e
Aman <amanshah2711@berkeley.edu>
2025-01-30 04:07:56 -0500

    fix arrow locations

--- frontend/src/DayManager.jsx
+++ frontend/src/DayManager.jsx
@@ -1,4 +1,6 @@
 
+import './DayManager.css';
+
 export default function DayManager({day, setDay}) {
     const nyDate = new Date('1993-11-21T00:00:00-05:00'); // 00:00 represents midnight in New York (UTC-5)
     const nyDateOnly = new Date(nyDate.setHours(0, 0, 0, 0));
@@ -23,10 +25,10 @@ export default function DayManager({day, setDay}) {
       day: "numeric",  
     });
     return (
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button" className="btn" onClick={onClick(-1)}>←</button>
-                <p className="h2 mb-0">{formatter.format(day)}</p>
-                <button type="button" className="btn" onClick={onClick(1)}>→</button>
+            <div className="date-container d-flex justify-content-center align-items-center col-4">
+                <button type="button" className="btn left-arrow" onClick={onClick(-1)}>←</button>
+                <p className="h2 mb-0 date">{formatter.format(day)}</p>
+                <button type="button" className="btn right-arrow" onClick={onClick(1)}>→</button>
             </div>
     )
 }
