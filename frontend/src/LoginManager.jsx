e7644d0f1b18e2e06b53a23ad9ed4930b353db3b
Aman <amanshah2711@berkeley.edu>
2025-03-26 17:18:39 -0400

    revert

--- frontend/src/LoginManager.jsx
+++ frontend/src/LoginManager.jsx
@@ -40,14 +40,14 @@ export default function LoginManager ({setShowRegistrationManager, message, setM
                     <form autoComplete="on">
                         <div className="form-group text-start mb-4">
                             <label htmlFor="email">Email address:</label>
-                            <input type="email" name="login" id="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Enter email"/>
+                            <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Enter email"/>
                         </div>
                         <div className="row">
                             <div></div>
                         </div>
                         <div className="form-group text-start mb-4">
                             <label htmlFor="password">Password:</label>
-                            <input type="password" name="password" id="password" autoComplete="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Password"/>
+                            <input type="password" name="password" id="password" autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Password"/>
                         </div>
                         <div className="row">
                             <p className="text-center text-secondary">
