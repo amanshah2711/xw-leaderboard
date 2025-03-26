cd38ff2e53fa469168bba924f7fc2459b20cb6a5
Aman <amanshah2711@berkeley.edu>
2025-03-26 17:56:19 -0400

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:'0.0.0.0',
-    port:5173,
+    host: '0.0.0.0',
+    port: 5173,
     allowedHosts: ['xwleaderboard.amanshah2711.me'],
-   proxy: process.env.NODE_ENV === 'development' ? {
-     '/api':  {
-          target: 'http://xwleaderboardbackend:5000',
-          changeOrigin: true,
-          secure: false,      
-          ws: false,
+    proxy: process.env.NODE_ENV === 'development' ? {
+      '/api': {
+        target: 'http://xwleaderboardbackend:5000',
+        changeOrigin: true,
+        secure: false,
       }
- } : {}
-
+    } : {},
   },
 });
