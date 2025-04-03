28991279bb754203b9f962b3a63d99ad20b783b8
Aman <amanshah2711@berkeley.edu>
2025-04-03 18:13:45 -0400

    expand date

--- frontend/src/DayManager.jsx
+++ frontend/src/DayManager.jsx
@@ -35,7 +35,7 @@ export default function DayManager({day, setDay}) {
     return (
             <div className="row justify-content-center align-items-center mb-2">
                     <button type="button" className="btn col-1 border-0" onClick={onClick(-1)}>←</button>
-                    <p className="h2 mb-0 col-4 text-break">{formatter.format(day)}</p>
+                    <p className="h2 mb-0 col-5 text-break">{formatter.format(day)}</p>
                     <button type="button" className="btn col-1 border-0" onClick={onClick(1)}>→</button>
             </div>
     )
