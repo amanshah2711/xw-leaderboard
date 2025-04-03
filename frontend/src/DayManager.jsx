870cc8aaf3b5f0746626204d4e183d8aa2a80ab0
Aman <amanshah2711@berkeley.edu>
2025-04-03 18:40:49 -0400

    stabilize button

--- frontend/src/DayManager.jsx
+++ frontend/src/DayManager.jsx
@@ -35,10 +35,10 @@ export default function DayManager({day, setDay}) {
     return (
             <div className="row justify-content-center align-items-center mb-2">
                 <div className="col-6 justify-content-center">
-                    <div className="d-flex flex-row me-2 ms-2 justify-content-between align-items-center">
-                        <button type="button" className="btn col-1 border-0" onClick={onClick(-1)}>←</button>
+                    <div className="d-flex flex-row justify-content-between align-items-center">
+                        <button type="button" className="btn border-0" onClick={onClick(-1)}>←</button>
                         <p className="h2 mb-0">{formatter.format(day)}</p>
-                        <button type="button" className="btn col-1 border-0" onClick={onClick(1)}>→</button>
+                        <button type="button" className="btn border-0" onClick={onClick(1)}>→</button>
                     </div>
                 </div>
             </div>
