91c41e0f20a7cced02afc6e477ef6ff579a19c59
Aman <amanshah2711@gmail.com>
2025-04-29 21:03:52 +0000

    fix form structure for safari autofill, put register in separate form

--- frontend/src/LoginManager.jsx
+++ frontend/src/LoginManager.jsx
@@ -25,29 +25,28 @@ export default function LoginManager ({setShowRegistrationManager, message, setM
             <div className="row justify-content-center">
                 <img src={icon} className="img-fluid" style={{ maxWidth: "300px", height: "auto" }}></img>
             </div>
-            <div className="row mb-2">
-                <div className="col-4"></div>
-                <div className="col-4 d-flex justify-content-center">
-                    <form method="post" autoComplete="on" onSubmit={handleSubmit}>
-                        <div className="text-start mb-4">
-                            <input className="form-control" name="username" type="email" id="username" autoCorrect="off" autoCapitalize="off" autoComplete="username" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
-                        </div>
-                        <div className="row">
-                        </div>
-                        <div className="text-start mb-4">
-                            <input className="form-control" name="password" type="password" id="password" autoCorrect="off" autoCapitalize="off" autoComplete="current-password"  value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
-                        </div>
-                        <button type="submit" className="btn btn-primary mx-2 mb-2">Submit</button>
-                        <button type="button" className="btn btn-secondary mx-2 mb-2" onClick={() => {setShowRegistrationManager(true);setMessage("")}}>Register</button>
-                    </form>
+                <div className="row mb-2">
+			<div className="col-4"></div>
+	    		<div className="col-4 d-flex flex-column justify-content-center">
+			    <form method="post" autoComplete="on">
+	    			<div className="form-group text-start mb-4">
+					<input id="user-text-field" type="email" autoComplete="username" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="Email" required/>
+	    			</div>
+	    			<div className="form-group text-start mb-4">
+					<input id="password-text-field" type="password" autoComplete="current-password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="Password" required/> 
+	    			</div>
+				<button type="submit" className="btn btn-primary mx-2 mb-2" onClick={handleSubmit}>Submit</button>
+			    </form>
+	    		    <form>
+	    			<button className="btn btn-secondary mx-2 mb-2" onClick={() => {setShowRegistrationManager(true);setMessage("")}}>Register</button>
+	    		    </form>
+	    		</div>
+			<div className="col-4"></div>
                 </div>
-                <div className="col-4"></div>
-            </div>
-                <div className="row">
-                    <p className="text-center text-secondary">
-                        {message}
-                    </p>
-                </div>
+	    	<div className="row">
+	    	<p className="text-center text-secondary">{message}</p>
+	    </div>
+	    	
         </div>
     )
 }
