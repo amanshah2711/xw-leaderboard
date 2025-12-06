ff0783dd2d28ab2c660f89cfe73fe417214ea5c9
Aman <amanshah2711@berkeley.edu>
2025-12-05 22:33:21 -0500

    updates to login for display name

--- backend/app/routes/auth/registration.py
+++ backend/app/routes/auth/registration.py
@@ -15,24 +15,26 @@ def register():
     form = RegistrationForm()
     if form.validate():
         data = request.get_json()
+        try:
+            display_name = data.get('displayName')
+            email = data.get('email')
+            password = data.get('password')
 
-        display_name = data.get('displayName')
-        email = data.get('email')
-        password = data.get('password')
+            invite_link = generate_link()
 
-        invite_link = generate_link()
-
-        if User.query.filter_by(email=email).first():
-            return jsonify({"success": False, "message": "Email is already registered"}), 200
-        user = User(display_name=display_name, email=email, invite_link=invite_link)
-        user.set_password(password)
-        success, info = create_and_send_verification_email(email, user.id)
-        if success:
-            db.session.add(user)
-            db.session.commit()
-            return jsonify({"success": True, "message": "Verify your email and log in."}), 200
-        else:
-            return jsonify({"success": False, "message": "We had an issue processing your email."}), 200
+            if User.query.filter_by(email=email).first():
+                return jsonify({"success": False, "message": "Email is already registered"}), 200
+            user = User(display_name=display_name, email=email, invite_link=invite_link)
+            user.set_password(password)
+            success, info = create_and_send_verification_email(email, user.id)
+            if success:
+                db.session.add(user)
+                db.session.commit()
+                return jsonify({"success": True, "message": "Verify your email and log in."}), 200
+            else:
+                return jsonify({"success": False, "message": "We had an issue processing your email."}), 200
+        except Exception as e:
+            return jsonify({"success": False, "message": "We had an issue processing your email."}), 200 
     else:
         message = ''
         for field, errors in form.errors.items():
